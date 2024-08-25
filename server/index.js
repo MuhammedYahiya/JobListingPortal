0205bba7e268f62e091024ee68fc80b607fc9019
MuhammedYahiya <yahiyahmhd@gmail.com>
2024-08-26 03:17:13 +0530

    connect mongodb

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const userRout = require('./router/userRouter')
+const connectDatabase = require('./config/database')
 const app = express()
+
 
 dotenv.config()
 
 app.use('/api',userRout)
 
+connectDatabase()
+
 const PORT = process.env.PORT || 5000
+
 
 app.listen(PORT, ()=>{
     console.log(`app listening on port ${PORT}`)
-})              
+})  
+
+
+
