a7aacc3e2ba52fd31aed0cbb31df7ebe70de042a
Tushar Jain <tusharjainqwerty@gmail.com>
2024-08-26 13:32:26 +0530

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Login from './Login/Login';
+
+
 
 
 function App() {
   return (
     <>
-      <Login/>
+    <Login/>
     </>
+
   );
 }
 
--- client/src/Login/Login.jsx
+++ client/src/Login/Login.jsx
@@ -1,33 +1,79 @@
 import React from "react";
 import Welcome from "./Welcome.jpg";
+import { useState } from "react";
+import JOB from "./Jobgenie.jpeg";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEmail("  ");
+    setPassword("");
+    setRole("");
+  };
   return (
     <div className="login-div flex flex-row">
-      <div className="login-left flex-1 p-4">
-        <span className="p-2 bg-slate-600 text-white font-serif font-semibold">
-          JobPortal
-        </span>
-        <div className="flex flex-row gap-4 justify-end p-4 font-serif font-semibold text-white">
-          <button className="bg-blue-600 p-2">POST A JOB</button>
-          <button className="bg-green-600 p-2">WANT A JOB</button>
-        </div>
+      <div className="login-left flex flex-1 p-4 justify-center items-center">
+        <form className="flex flex-col gap-8  p-4 " onSubmit={handleSubmit}>
+          <div className="flex justify-start">
+            <img className="h-10 w-10" src={JOB} alt="" />
+            <div className="font-semibold font-serif text-blue-600 tracking-wide">
+              JobGene
+            </div>
+          </div>
 
-        <div className="flex font-bold text-5xl justify-center mt-20 text-gray-800 italic font-serif ">
-          Create an account{" "}
-        </div>
-        <div className="flex font-bold text-xl justify-center mt-5 text-gray-700 italic font-serif">
-          {" "}
-          for tools to help you
-        </div>
-        <div className="flex flex-col mt-10 gap-5">
-          <button className=" border-gray-500  border-2 text-2xl font-bold p-3 text-blue-500 tracking-wider">
-            Employer
+          <div className="font-bold text-5xl justify-center text-gray-800 italic font-serif ml-[100px]">
+            Login
+          </div>
+
+          <div className=" flex gap-2">
+            <label className="font-bold">Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border-gray-500 border-2 rounded-lg w-full pl-2 font-medium"
+              required
+            />
+          </div>
+          <div className=" flex gap-2">
+            <label className="font-bold">Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border-gray-500 border-2 rounded-lg w-full pl-2 font-medium"
+              required
+            />
+          </div>
+          <div className=" flex gap-2">
+            <label className="font-bold">Role:</label>
+            <select
+              className="border-gray-500 border-2 rounded-lg w-full pl-2 font-medium"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+            >
+              <option value="Select"></option>
+              <option value="jobSeeker">Job Seeker</option>
+              <option value="employer">Employer</option>
+            </select>
+          </div>
+          <button
+            className="text-white bg-green-500 p-3 font-bold tracking-wider"
+            type="submit"
+          >
+            Login
           </button>
-          <button className=" border-gray-500 border-2 text-2xl font-bold p-3 text-blue-500 tracking-wider">
-            Jobseeker
-          </button>
-        </div>
+          <div className="flex flex-row gap-2 text-blue-500 font-medium">
+            Don't have an account?
+            <button className="text-white bg-blue-500 p-2">
+              Register here.
+            </button>
+          </div>
+        </form>
       </div>
       <div className="login-right flex-1 p-4">
         <img className="" src={Welcome} alt="" />
