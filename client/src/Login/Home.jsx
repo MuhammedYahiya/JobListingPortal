2042a94d4897b59a89f0bbd339b77a722976bae6
unknown <fizasoorajkhangmail.com>
2024-08-27 16:51:24 +0530

    added emplyeer side

--- client/src/Login/Home.jsx
+++ client/src/Login/Home.jsx
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Home.css'
+import welcome from '../assests/Welcome.png'
 
 function Home() {
   const [role, setRole] = useState('');
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    if (role) {
+    if (!role) {
+      alert("Please select a role before proceeding.");
+    } else {
       navigate(`/login?role=${role}`);
     }
   };
 
   const handleRegister = () => {
-    if (role) {
+    if (!role) {
+      alert("Please select a role before proceeding.");
+    } else {
+
       navigate(`/register?role=${role}`);
     }
   };
@@ -20,13 +27,17 @@ function Home() {
   return (
     <div className="home-container">
       <h1>Welcome to Job Portal</h1>
+      <img src={welcome}/>
+
+  
       <div className="role-selector">
-        <label>
+      <label>
           <input
             type="radio"
             value="employer"
             checked={role === 'employer'}
             onChange={(e) => setRole(e.target.value)}
+            aria-label="Select Employer role"
           />
           Employer
         </label>
@@ -36,9 +47,11 @@ function Home() {
             value="jobseeker"
             checked={role === 'jobseeker'}
             onChange={(e) => setRole(e.target.value)}
+            aria-label="Select Job Seeker role"
           />
           Job Seeker
         </label>
+
       </div>
       <div className="action-buttons">
         <button onClick={handleLogin} disabled={!role}>
