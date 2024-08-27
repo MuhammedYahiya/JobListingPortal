e59d0680b8bdc3436ad6c462cff500006032b473
Tushar Jain <tusharjainqwerty@gmail.com>
2024-08-28 01:12:14 +0530

    gettingvalues

--- client/src/Login/Login.jsx
+++ client/src/Login/Login.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
@@ -5,13 +6,22 @@ function Login() {
   const search = useLocation().search;
   const role = new URLSearchParams(search).get('role');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    
+    e.target.reset()
+  }
+
   return (
     <div className="login-container">
       <h2>{role === 'employer' ? 'Employer' : 'Job Seeker'} Login</h2>
-      <form>
-        <input type="email" placeholder="Email" required />
-        <input type="password" placeholder="Password" required />
-        <button type="submit">Login</button>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder="Email" required />
+        <input type="password" name="password" placeholder="Password" required />
+        <button type="submit" >Login</button>
       </form>
     </div>
   );
