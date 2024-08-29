91dccbd47e9cb73b73ef6c73b2ac3a68d2d8b379
FIZA SOORAJ KHAN <fizasoorajkhangmail.com>
2024-08-29 21:05:59 +0530

    navbar

--- client/src/Login/Navbar/Navbar.jsx
+++ client/src/Login/Navbar/Navbar.jsx
@@ -1,16 +1,20 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // Add styling for the navbar
+import './Navbar.css'; 
+import logo from '../../assests/Jobgenie.jpeg'
 
 function Navbar() {
   return (
     <nav className="navbar">
+      <div className="logo">
+          
+      <img src={logo} style={{ height: '70px', width: 'auto' }}/></div>
       <ul className="navbar-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
-        {/* Add more links as needed */}
+        
       </ul>
     </nav>
   );
