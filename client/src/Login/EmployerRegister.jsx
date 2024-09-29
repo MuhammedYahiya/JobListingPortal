a6ba6b0de152e8fc1c48b8e6b303d59e656ff1ee
Tushar Jain <tusharjainqwerty@gmail.com>
2024-09-29 12:42:13 +0530

    updated

--- client/src/Login/EmployerRegister.jsx
+++ client/src/Login/EmployerRegister.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./Register.css";
+
 import { useState } from "react";
 
 function EmployerRegister() {
@@ -61,141 +61,172 @@ function EmployerRegister() {
   };
 
   return (
-    <div className=" flex flex-col justify-center items-center w-full min-h-[100vh]">
-      {/* container-full */}
-      <div className=" flex flex-col justify-center items-center border-4 p-5  min-h-[100vh]">
-      {/* register-container */}
-        <div className="text-2xl">Employer Registration</div>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Your Profile Pic to be displayed
-            <input
-              type="file"
-              name="profilePicture"
-              onChange={handleImageChange}
-              required
-            />
-          </label>
-          <label>
-            Company Name*
-            <input
-              type="text"
-              name="companyName"
-              required
-              placeholder="e.g., Amazon"
-            />
-          </label>
-          <label>
-            Your Name*
-            <input
-              type="text"
-              name="name"
-              required
-              placeholder="e.g., John Doe"
-            />
-          </label>
-          <label>
-            Company Address*
-            <input
-              type="text"
-              name="address"
-              required
-              placeholder="eg: 1234 Elm St"
-            />
-          </label>
+    <div className="p-4 ">
+      <div className=" flex flex-col justify-center items-center w-full h-screen  ">
+        <div className=" flex flex-col  border-4 p-5 overflow-y-scroll  min-h-[100vh] border-gray-300 custom-scrollbar w-[70%] shadow-[2px_2px_5px_1px_rgba(0,0,0,0.3)]">
+          <div className="flex text-4xl font-bold text-zinc-500 p-2 mb-5 text-stroke-3 bg-gray-600 tracking-wider justify-center">
+            Employer Registration
+          </div>
+          <form onSubmit={handleSubmit} className="flex items-center justify-center">
+            <label className="font-semibold text-red-900">
+              Your Profile Pic to be displayed
+              <input
+                type="file"
+                name="profilePicture"
+                onChange={handleImageChange}
+                required
+                className="text-gray-600 "
+              />
+            </label>
+            <label className="font-semibold text-blue-800">
+              Company Name*
+              <input
+                type="text"
+                name="companyName"
+                required
+                placeholder="e.g., Amazon"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
+            <label className="font-semibold text-blue-800">
+              Your Name*
+              <input
+                type="text"
+                name="name"
+                required
+                placeholder="e.g., John Doe"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
+            <label className="font-semibold text-blue-800">
+              Company Address*
+              <input
+                type="text"
+                name="address"
+                required
+                placeholder="eg: 1234 Elm St"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
 
-          <label>
-            City*
-            <input
-              type="text"
-              name="city"
-              required
-              placeholder="eg: Springfield"
-            />
-          </label>
+            <label className="font-semibold text-blue-800">
+              City*
+              <input
+                type="text"
+                name="city"
+                required
+                placeholder="eg: Springfield"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
 
-          <label>
-            State*
-            <input type="text" name="state" required placeholder="eg: IL" />
-          </label>
+            <label className="font-semibold text-blue-800">
+              State*
+              <input
+                type="text"
+                name="state"
+                required
+                placeholder="eg: IL"
+                className=" text-gray-500 tracking-wider font-normal font-serif outline-gray-400"
+              />
+            </label>
 
-          <label>
-            Country*
-            <input type="text" name="country" required placeholder="eg: USA" />
-          </label>
+            <label className="font-semibold text-blue-800">
+              Country*
+              <input
+                type="text"
+                name="country"
+                required
+                placeholder="eg: USA"
+                className=" font-normal text-gray-500 tracking-wider font-serif outline-gray-400"
+              />
+            </label>
 
-          <label>
-            Pin Code*
-            <input
-              type="text"
-              name="pinCode"
-              required
-              placeholder="eg: 12345"
-            />
-          </label>
+            <label className="font-semibold text-blue-800">
+              Pin Code*
+              <input
+                type="text"
+                name="pinCode"
+                required
+                placeholder="eg: 12345"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
 
-          <label>
-            Types of Position*
-            <select name="positionType" required>
-              <option value="fullTime">Full Time</option>
-              <option value="partTime">Part Time</option>
-              <option value="contract">Contract</option>
-              <option value="internship">Internship</option>
-            </select>
-          </label>
+            <label className="font-semibold text-blue-800">
+              Types of Position*
+              <select name="positionType" required>
+                <option value="fullTime">Full Time</option>
+                <option value="partTime">Part Time</option>
+                <option value="contract">Contract</option>
+                <option value="internship">Internship</option>
+              </select>
+            </label>
 
-          <label>
-            Social Media Link
-            <input
-              type="url"
-              name="socialMediaLink"
-              placeholder="https://www.linkedin.com/company/amazon/jobs/"
-            />
-          </label>
+            <label className="font-semibold text-blue-800">
+              Social Media Link
+              <input
+                type="url"
+                name="socialMediaLink"
+                placeholder="https://www.linkedin.com/company/amazon/jobs/"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
 
-          <label>
-            Are you a hiring manager?
-            <select name="hiringManager">
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
-            </select>
-          </label>
-          <label>
-            Your phone number
-            <input
-              type="tel"
-              name="phone"
-              required
-              placeholder="eg: 09306695193"
-            />
-          </label>
-          <label>
-            Your company's number of employees
-            <select name="employees">
-              <option value="1-10">1 to 10</option>
-              <option value="11-50">11 to 50</option>
-              <option value="51-149">50 to 149</option>
-              <option value="150+">150+</option>
-            </select>
-          </label>
-          <label>
-            Email Address*
-            <input
-              type="email"
-              name="email"
-              required
-              placeholder="e.g., john@company.com"
-            />
-          </label>
-          <label>
-            Password*
-            <input type="password" name="password" required />
-          </label>
+            <label className="font-semibold text-blue-800">
+              Are you a hiring manager?
+              <select name="hiringManager">
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+              </select>
+            </label>
+            <label className="font-semibold text-blue-800">
+              Your phone number
+              <input
+                type="tel"
+                name="phone"
+                required
+                placeholder="eg: 09306695193"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
+            <label className="font-semibold text-blue-800">
+              Your company's number of employees
+              <select name="employees">
+                <option value="1-10">1 to 10</option>
+                <option value="11-50">11 to 50</option>
+                <option value="51-149">50 to 149</option>
+                <option value="150+">150+</option>
+              </select>
+            </label>
+            <label className="font-semibold text-blue-800">
+              Email Address*
+              <input
+                type="email"
+                name="email"
+                required
+                placeholder="e.g., john@company.com"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
+            <label className="font-semibold text-blue-800">
+              Password*
+              <input
+                type="password"
+                name="password"
+                required
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
 
-          <button type="submit" className="continue-button">
-            Register
-          </button>
-        </form>
+            <button
+              type="submit"
+              className=" flex justify-center font-semibold"
+            >
+              Register
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
--- client/src/Login/JobSeekerRegister.jsx
+++ client/src/Login/JobSeekerRegister.jsx
@@ -17,20 +17,23 @@ function JobSeekerRegister() {
     // }
 
     try {
-      const response = await axios.post("http://localhost:8000/api/jobseeker/register", {
-        email: data.email,
-        password: data.password,
-        name: data.name,
-        address: data.address,
-        city: data.city,
-        state: data.state,
-        country: data.country,
-        pincode: data.pincode,
-        socialMediaLink: data.socialMediaLink,
-        jobtitlename: data.jobtitlename,
-        experience: data.experience,
-        positionType: data.positionType,
-      });
+      const response = await axios.post(
+        "http://localhost:8000/api/jobseeker/register",
+        {
+          email: data.email,
+          password: data.password,
+          name: data.name,
+          address: data.address,
+          city: data.city,
+          state: data.state,
+          country: data.country,
+          pincode: data.pincode,
+          socialMediaLink: data.socialMediaLink,
+          jobtitlename: data.jobtitlename,
+          experience: data.experience,
+          positionType: data.positionType,
+        }
+      );
 
       if (response.status === 200) {
         alert("Job Seeker registered successfully");
@@ -45,130 +48,136 @@ function JobSeekerRegister() {
   };
 
   return (
-    <div className="container-full min-h-[100vh]">
-      <div className="register-container min-h-[100vh]">
-        <h2>Job Seeker Registration</h2>
-        <form onSubmit={handleSubmit}>
+    <div  className="p-4 ">
+      <div className="flex flex-col justify-center items-center w-full h-screen">
+        <div className=" flex flex-col  border-4 p-5 overflow-y-scroll  min-h-[100vh] border-gray-300 custom-scrollbar w-[70%] shadow-[2px_2px_5px_1px_rgba(0,0,0,0.3)]">
+          <div className="flex text-4xl font-bold text-zinc-500 p-2 mb-5 text-stroke-3 bg-gray-600 tracking-wider justify-center">Job Seeker Registration</div>
+          <form onSubmit={handleSubmit} className="flex items-center justify-center">
+            <label className="font-semibold text-blue-800">
+              Your Name*
+              <input
+                type="text"
+                name="name"
+                required
+                placeholder="e.g., Jane Doe"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
 
-          {/* <label>
-            Profile Photo
-            <input type="file" name="profilePhoto" accept="image/*" />
-          </label> */}
+            <label className="font-semibold text-blue-800">
+              Address*
+              <input
+                type="text"
+                name="address"
+                required
+                placeholder="eg: 1234 Elm St"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
 
+            <label className="font-semibold text-blue-800">
+              City*
+              <input
+                type="text"
+                name="city"
+                required
+                placeholder="eg: Springfield"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
 
-          <label>
-            Your Name*
-            <input
-              type="text"
-              name="name"
-              required
-              placeholder="e.g., Jane Doe"
-            />
-          </label>
+            <label className="font-semibold text-blue-800">
+              State*
+              <input type="text" name="state" required placeholder="eg: IL" className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"/>
+            </label>
 
-          <label>
-            Address*
-            <input
-              type="text"
-              name="address"
-              required
-              placeholder="eg: 1234 Elm St"
-            />
-          </label>
+            <label className="font-semibold text-blue-800">
+              Country*
+              <input
+                type="text"
+                name="country"
+                required
+                placeholder="eg: USA"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
 
-          <label>
-            City*
-            <input
-              type="text"
-              name="city"
-              required
-              placeholder="eg: Springfield"
-            />
-          </label>
+            <label className="font-semibold text-blue-800">
+              Pin Code*
+              <input
+                type="text"
+                name="pinCode"
+                required
+                placeholder="eg: 12345"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
 
-          <label>
-            State*
-            <input type="text" name="state" required placeholder="eg: IL" />
-          </label>
+            <label className="font-semibold text-blue-800">
+              Years of Experience
+              <select name="experience">
+                <option value="0-1">0 to 1</option>
+                <option value="1-2">1 to 2</option>
+                <option value="3-5">3 to 5</option>
+                <option value="5-10">5 to 8</option>
+              </select>
+            </label>
 
-          <label>
-            Country*
-            <input type="text" name="country" required placeholder="eg: USA" />
-          </label>
+            <label className="font-semibold text-blue-800">
+              Job Title*
+              <input
+                type="text"
+                name="jobtitlename"
+                required
+                placeholder="eg: software engineer"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
 
-          <label>
-            Pin Code*
-            <input
-              type="text"
-              name="pinCode"
-              required
-              placeholder="eg: 12345"
-            />
-          </label>
+            <label className="font-semibold text-blue-800">
+              Types of Position*
+              <select name="positionType" required>
+                <option value="fullTime">Full Time</option>
+                <option value="partTime">Part Time</option>
+                <option value="contract">Contract</option>
+                <option value="internship">Internship</option>
+              </select>
+            </label>
 
+            <label className="font-semibold text-blue-800">
+              Social Media Link
+              <input
+                type="url"
+                name="socialMediaLink"
+                placeholder="https://www.linkedin.com/company/amazon/jobs/"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
+            <label className="font-semibold text-blue-800">
+              Email Address*
+              <input
+                type="email"
+                name="email"
+                required
+                placeholder="e.g., jane@example.com"
+                className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"
+              />
+            </label>
 
+            <label className="font-semibold text-blue-800">
+              Resume
+              <input type="file" name="resume" accept=".pdf,.doc,.docx" className=" text-gray-500 tracking-wider font-serif font-normal outline-gray-400"/>
+            </label>
 
-          <label>
-            Years of Experience
-            <select name="experience">
-              <option value="0-1">0 to 1</option>
-              <option value="1-2">1 to 2</option>
-              <option value="3-5">3 to 5</option>
-              <option value="5-10">5 to 8</option>
-            </select>
-          </label>
-
-          <label>
-            Job Title*
-            <input
-              type="text"
-              name="jobtitlename"
-              required
-              placeholder="eg: software engineer"
-            />
-          </label>
-
-          <label>
-            Types of Position*
-            <select name="positionType" required>
-              <option value="fullTime">Full Time</option>
-              <option value="partTime">Part Time</option>
-              <option value="contract">Contract</option>
-              <option value="internship">Internship</option>
-            </select>
-          </label>
-
-          <label>
-            Social Media Link
-            <input
-              type="url"
-              name="socialMediaLink"
-              placeholder="https://www.linkedin.com/company/amazon/jobs/"
-            />
-          </label>
-          <label>
-            Email Address*
-            <input
-              type="email"
-              name="email"
-              required
-              placeholder="e.g., jane@example.com"
-            />
-          </label>
-
-          <label>
-            Resume
-            <input type="file" name="resume" accept=".pdf,.doc,.docx" />
-          </label>
-          
-          <label>
-            Password*
-            <input type="password" name="password" required />
-          </label>
-          <button type="submit" className="continue-button">
-            Register
-          </button>
-        </form>
+            <label className="font-semibold text-blue-800">
+              Password*
+              <input type="password" name="password" required />
+            </label>
+            <button type="submit" className="font-semibold">
+              Register
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
